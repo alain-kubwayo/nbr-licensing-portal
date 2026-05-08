c03bcf47f96e73f93f303df5268dc7b9aa70dfde
alain-kubwayo <ahkubwayo@gmail.com>
2026-05-08 15:41:25 +0200

    chore: setup backend

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Backend is ready." });
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
