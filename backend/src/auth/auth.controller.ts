a12b573536cdbe55b9587697b006d395ae124ff6
alain-kubwayo <ahkubwayo@gmail.com>
2026-05-09 21:12:43 +0200

    feat: add role-based access control to the application

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ExtractUserFromRequest } from '../common/decorators/extract-user.decorator';
+import { RequiredRoles } from '../common/decorators/required-roles.decorator';
 import { GenericResponse } from '../common/utils/http.utils';
+import { UserRole } from '../users/enums/user-role.enum';
 import { UserEntity } from '../users/user.entity';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -17,7 +20,8 @@ export class AuthController {
   }
 
   @Get('me')
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @RequiredRoles(UserRole.REVIEWER)
   getProfile(@ExtractUserFromRequest() user: UserEntity) {
     return new GenericResponse('Profile retrieved successfully', user);
   }
