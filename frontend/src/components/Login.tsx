9c131d475e66300c784dee948d0d1944fe373bfe
alain-kubwayo <ahkubwayo@gmail.com>
2026-05-09 20:28:27 +0200

    feat: setup login page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,57 +1,57 @@
-import type { FormEvent } from "react";
-import { Button } from "./ui/button";
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
-import { Label } from "./ui/label";
+import { useState, type FormEvent } from "react";
+import FormWrapper from "./FormWrapper";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 import { Link } from "react-router";
 
+type LoginData = {
+    email: string
+    password: string
+}
 
-const Login = () => {
-  function onSubmit(e: FormEvent) {
-    e.preventDefault();
+const INITIAL_DATA: LoginData = {
+  email: "",
+  password: ""
+}
 
-    // POST data to log account in
-    console.log("Successful account login");
-  }
+type LoginFormProps = LoginData & {
+    updateFields: (fields: Partial<LoginData>) => void
+}
+
+const Login = ({ email, password }: LoginFormProps) => {
+    const [data, setData] = useState(INITIAL_DATA);
+    function updateFields(fields: Partial<LoginData>) {
+        setData(prev => {
+        return {...prev, ...fields}
+        })
+    }
+
+    function onSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        // POST data to login into the account account
+        console.log("Successful login", data);
+    }
 
   return (
-    <Card className="w-full max-w-md mx-auto m-4 rounded-2xl w-full max-w-md mx-auto">
-        <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>
-                Enter your credentials to continue
-            </CardDescription>
-            <CardAction>
-                <Link to="/register">Create Account</Link>
-            </CardAction>
-        </CardHeader>
-        <CardContent>
-            <form onSubmit={onSubmit}>
-                <div className="flex flex-col gap-6">
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                            type="email"
-                            required
-                        />
-                    </div>
-                    <div className="grid gap-2">
-                        <Label htmlFor="password">Password</Label>
-                        <Input
-                            type="password"
-                            required
-                        />
-                    </div>
-                </div>
-            </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-            <Button type="submit" className="w-full">
-                Login
-            </Button>
-        </CardFooter>
-    </Card>
+    <div className="relative bg-white border border-black p-4 m-4 rounded-2xl w-full max-w-md mx-auto">
+        <form onSubmit={onSubmit}>
+        <FormWrapper title="Login">
+            <label>Email</label>
+            <Input autoFocus required type="email" value={email} onChange={e => updateFields({ email: e.target.value })} />
+            <label>Password</label>
+            <Input required type="password" value={password} onChange={e => updateFields({ password: e.target.value })} />
+            <p className="flex items-end">
+                {/* No account yet? */}
+                <Link className="ml-2 underline text-blue-700" to="/register">Register</Link>
+            </p>
+            <div className="mt-4 flex w-full justify-end">
+                <Button type="submit">Login</Button>
+            </div>
+        </FormWrapper>
+        </form>
+    </div>
   )
 }
 
-export default Login
+export default Login;
